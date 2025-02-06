2fdeb402f5d2e7bf20370804be77745628ce3ae7
Le Kieu Trang <104716412+kieutrangdev@users.noreply.github.com>
2025-02-06 19:30:03 +0700

    create Keyboard

---
+++ src/main/java/handleKeyboardTagWindows/KeyBoardAction.java
@@ -0,0 +1,18 @@
+package handleKeyboardTagWindows;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class KeyBoardAction{
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://text-compare.com/");
+        driver.manage().window().maximize();
+        Actions act = new Actions(driver);
+
+
+    }
+}
